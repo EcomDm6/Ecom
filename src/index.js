d7303372f63de330bd34b2af48c1ea8391b027ee
SheaClose <SheaClose@gmail.com>
2017-09-20 07:35:44 -0500

    added linting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // @flow
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App.jsx';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDom.render(<App />, document.getElementById('root'));
+});
