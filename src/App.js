a76d9c5cda3037867efd5cc5dbd5a302ac9748f2
Brobbiethao <Brobbiethao@gmail.com>
2017-09-21 10:14:27 -0700

    Main Components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import React from 'react';
 
 import Nav from './Components/Nav/NavComponent';
+import SaleComponent from './Components/SalePreview/SaleComponent';
+import FooterContainer from './Components/InfoFooter/FooterComponent';
 import './style.css';
 
 const App = () => (
   <div>
     <Nav />
-    <div className="">Working App!</div>
+    <SaleComponent />
+    <FooterContainer />
   </div>
 );
 
